4e40d3b652e1b4c2b90546d992e725a3571ed852
maksymPtashnyk <super.zet65@gmail.com>
2023-12-18 20:44:39 +0200

    added styles for form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -6,9 +6,14 @@ import fetchPositions from '../Services/positionService';
 import { UserContext } from '../../context/Context';
 import Loader from '../Loader/Loader';
 import Modal from '../Modal/Modal';
+import styles from './RegistrationForm.module.scss'
 
 const RegistrationForm = () => {
-  const { register, handleSubmit, formState: { isValid } } = useForm({ mode: 'onChange' });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: 'onChange' });
   const { setIsRegistration } = useContext(UserContext);
 
   const [token, setToken] = useState('');
@@ -77,47 +82,77 @@ const RegistrationForm = () => {
   return (
     <>
       {isModalOpen && <Modal closeModal={closeModal} />}
-      <form onSubmit={handleSubmit(onSubmit)} noValidate aria-label="Registration form">
-        <input
-          type="text"
-          {...register('name', { required: true, minLength: 2, maxLength: 60 })}
-          placeholder="Name"
-          aria-label="Name"
-        />
-        <input
-          type="email"
-          {...register('email', { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
-          placeholder="Email"
-          aria-label="Email"
-        />
-        <input
-          type="tel"
-          {...register('phone', { required: true, pattern: /^\+380\d{9}$/ })}
-          placeholder="Phone"
-          aria-label="Phone"
-        />
-        {
-          positions.map(position => (
-            <label key={position.id} aria-label={position.name}>
-              {position.name}
-              <input
-                type="radio"
-                {...register('position_id', { required: true })}
-                value={position.id}
-                aria-label={position.name}
-              />
-            </label>
-          ))
-        }
-        <input
-          type="file"
-          {...register('photo', { required: true, validate: (fileList) => fileList && fileList[0]?.type === 'image/jpeg' })}
-          accept="image/jpeg"
-          aria-label="Photo upload"
-        />
-        <button type="submit" disabled={!isValid || isSubmitting} aria-label="Register button">Register</button>
-      </form>
-      {isSubmitting && <Loader />}
+      <form onSubmit={handleSubmit(onSubmit)} noValidate className={styles.form} aria-label="Registration form">
+        <div className={styles.form__inputs}>
+          <div className={errors.name && styles.error}>
+            <input
+              type="text"
+              {...register('name', { required: 'Name is required', minLength: { value: 2, message: 'Minimum 2 characters' }, maxLength: { value: 60, message: 'Maximum 60 characters' } })}
+              placeholder="Your name"
+              aria-label="Name"
+              className={styles.form__input}
+            />
+            <p>{errors.name?.message}</p>
+          </div>
+          <div className={errors.email && styles.error}>
+            <input
+              type="email"
+              {...register('email', { required: 'Email is required', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Invalid email address" } })}
+              placeholder="Email"
+              aria-label="Email"
+              className={styles.form__input}
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div className={errors.phone && styles.error}>
+            <input
+              type="tel"
+              {...register('phone', { required: 'Phone is required', pattern: { value: /^\+380\d{9}$/, message: "Invalid phone number" } })}
+              placeholder="Phone"
+              aria-label="Phone"
+              className={styles.form__input}
+            />
+            <p>{errors.phone?.message}</p>
+          </div>
+        </div>
+
+        <fieldset>
+          <legend>Select your position</legend>
+          {
+            positions.map((position, index) => (
+              <label key={position.id} className={styles.lable}>
+                <input
+                  type="radio"
+                  {...register('position_id', { required: 'Position is required' })}
+                  value={position.id}
+                  defaultChecked={index === 0}
+                  aria-label={position.name}
+                  id={`radio-${position.id}`}
+                  className={styles.radio__hidden}
+                />
+                <span className={styles.radio__custom} />
+                {position.name}
+              </label>
+            ))
+          }
+        </fieldset>
+        <div className={errors.photo && styles.error}>
+          <input
+            type="file"
+            {...register('photo', {
+              required: 'Photo is required',
+              validate: (fileList) => (fileList && fileList[0]) ? fileList[0].type === 'image/jpeg' || 'Only jpeg files are accepted' : 'File is required'
+            })}
+            accept="image/jpeg"
+            aria-label="Photo upload"
+          />
+          <p>{errors.photo?.message}</p>
+        </div>
+
+        <button type="submit" className={styles.button} disabled={!isValid || isSubmitting} aria-label="Register button">Sign up</button>
+      </form >
+      {isSubmitting && <Loader />
+      }
     </>
   );
 };
